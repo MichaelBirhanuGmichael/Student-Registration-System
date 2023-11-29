aad3bc366b3444ec43f8ed6672dbbfc9f3fd2c30
BIRHANU <michaelbirhanualem@gmail.com>
2023-11-29 10:32:36 +0300

    unit test v2

--- StudentRegistrationSystem/UnitTest.java
+++ StudentRegistrationSystem/UnitTest.java
@@ -8,7 +8,7 @@ class UnitTest {
     @Test
     void generateCourseTable() {
         // Create a sample student
-        Student student = new Student("John Doe", "4965/14","Software Engineering", 1, 2);
+        Student student = new Student("John Doe", "2389/14","Software Engineering", 1, 2);
 
         // Generate the course table
         List<Course> courseTable = Course.generateCourseTable(student);
@@ -22,11 +22,11 @@ class UnitTest {
         @Test
     void testStudentConstructorAndGetters() {
         // Create a sample student
-        Student student = new Student("John Doe", "12345", "Software Engineering", 2, 1);
+        Student student = new Student("John Doe", "4965/14", "Software Engineering", 2, 1);
 
         // Validate the constructor and getters
         assertEquals("John Doe", student.getName());
-        assertEquals("1324/14", student.getId());
+        assertEquals("4965/14", student.getId());
         assertEquals("Software Engineering", student.getDep());
         assertEquals(2, student.getYear());
         assertEquals(1, student.getSemester());
@@ -39,14 +39,14 @@ class UnitTest {
 
         // Use setters to set values
         student.setName("Jane Doe");
-        student.setId("1324/14");
+        student.setId("1234/14");
         student.setDep("Computer Science");
         student.setYear(3);
         student.setSemester(2);
 
         // Validate the setters
         assertEquals("Jane Doe", student.getName());
-        assertEquals("54321", student.getId());
+        assertEquals("1234/14", student.getId());
         assertEquals("Computer Science", student.getDep());
         assertEquals(3, student.getYear());
         assertEquals(2, student.getSemester());
