4238feaf8315a9bf25449c0bee570e4a22a5b448
BIRHANU <michaelbirhanualem@gmail.com>
2023-11-29 11:14:24 +0300

    used file handling to store student info

--- StudentRegistrationSystem/Main.java
+++ StudentRegistrationSystem/Main.java
@@ -4,8 +4,32 @@ import java.util.List;
 
 import java.util.InputMismatchException;
 
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+
+    // Existing code...
+
+    private static void writeStudentToFile(Student student) {
+        String fileName = "StudentRegistrationSystem/student_info.txt";
+
+        try (FileWriter writer = new FileWriter(fileName ,true)) {
+            writer.write("\n-----------------------\n");
+            writer.write("Name: " + student.getName() + "\n");
+            writer.write("ID: " + student.getId() + "\n");
+            writer.write("Department: " + student.getDep() + "\n");
+            writer.write("Year: " + student.getYear() + "\n");
+            writer.write("Semester: " + student.getSemester() + "\n");
+
+            System.out.println("Student information has been written to " + fileName);
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
+
+
+
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             System.out.println("************************ Welcome to Haramaya University Student Registration System ******************************** ");
@@ -13,7 +37,7 @@ public class Main {
 
             boolean continueRegistration = true;
             while (continueRegistration) {
-                System.out.println("1.Collect Slip\n2.Exit");
+                System.out.println("1.Collect Slip\n2.Save to File\n3.Exit");
                 int answer = input.nextInt();
 
                 switch (answer) {
@@ -21,16 +45,20 @@ public class Main {
                         displayRegistrationSlip(student);
                         break;
                     case 2:
+                        writeStudentToFile(student);
+                        break;
+                    case 3:
                         continueRegistration = false;
                         break;
                     default:
-                        System.out.println("Invalid choice. Please enter 1 or 2.");
+                        System.out.println("Invalid choice. Please enter 1, 2, or 3.");
                 }
             }
         } catch (Exception e) {
             System.out.println("An unexpected error occurred: " + e.getMessage());
         }
     }
+
 
     private static Student registerStudent(Scanner input) {
         System.out.print("Full name: ");
